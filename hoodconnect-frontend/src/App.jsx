aaecaf78b6c9e794b6e385e8a481088abc467f92
dsouzajean27 <dsouzajean27@gmail.com>
2026-04-08 23:24:23 +0530

    update

--- hoodconnect-frontend/src/App.jsx
+++ hoodconnect-frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
-import Dashboard from "./pages/Profile";
+import Profile from "./pages/Profile";
 import ProtectedRoute from "./components/ProtectedRoute";
 import "leaflet/dist/leaflet.css";
 
